06ef696df71269dc47ca5e3314c0eb61693cd343
Ichrak19 <Ichrak19@192.168.10.3>
2015-10-28 10:36:02 +0100

    first commit
    Signed-off-by: Ichrak19 <Ichrak19@192.168.10.3>

---
+++ greeter/src/test/java/edu/esprit/greeter/test/GreederTest.java
@@ -0,0 +1,34 @@
+package edu.esprit.greeter.test;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.Before;
+
+import edu.esprit.greeter.Greeter;
+public class GreederTest {
+	Greeter greeter;
+	
+	@Before
+	public void setUp(){
+		greeter=new Greeter();
+	}
+	@Test 
+	public void itShouldSayHelloWorld(){
+		
+		String result=greeter.sayHello("world");
+		Assert.assertEquals("helloworld",result);
+		
+	}
+	@Test (expected=IllegalArgumentException.class)
+	public void itShouldRaiseException(){
+		String param="MAN";
+		greeter.sayHello(param);
+	}
+	@After
+	public void tearDown(){
+		greeter=null;
+	}
+	
+
+}
